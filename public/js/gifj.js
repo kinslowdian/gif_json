a2b2b1f708830b8d7971975b322cfb148974b184
Kinslow <simon.kinslow@aa7778.weu.grey.global>
2017-05-23 15:19:46 +0100

    First commit
    Basic json load function added along with data url

---
+++ public/js/gifj.js
@@ -0,0 +1,44 @@
+// DEBUG
+var trace = function(msg){ console.log(msg); };
+
+var ram;
+
+function pageLoad_init()
+{
+	trace("pageLoad_init();");
+
+	build();
+}
+
+function build()
+{
+	ram = {};
+	ram.url = "http://api.giphy.com/v1/gifs/search?q=funny+cat&api_key=dc6zaTOxFJmzC";
+	ram.data = {};
+
+	data_load(ram.url, data_found);
+}
+
+function data_load(file, callback)
+{
+	var xobj = new XMLHttpRequest();
+
+	xobj.overrideMimeType("application/json");
+	xobj.open('GET', file, true);
+	xobj.onreadystatechange = function()
+	{
+		if(xobj.readyState == 4 && xobj.status == "200")
+		{
+			callback(xobj.responseText);
+		}
+	};
+
+	xobj.send(null);
+}
+
+function data_found(data)
+{
+	ram.data = JSON.parse(data);
+
+	trace(ram.data);
+}
