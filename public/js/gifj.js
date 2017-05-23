2124376d884a5c73879726cfa21bbbff82109a4b
Kinslow <simon.kinslow@aa7778.weu.grey.global>
2017-05-23 15:26:42 +0100

    Simplified ram object
    No defining data object in ram as an object on build

--- public/js/gifj.js
+++ public/js/gifj.js
@@ -14,7 +14,7 @@ function build()
 {
 	ram = {};
 	ram.url = "https://api.giphy.com/v1/gifs/search?q=funny+cat&api_key=dc6zaTOxFJmzC";
-	ram.data = {};
+	// ram.data = {};
 
 	data_load(ram.url, data_found);
 }
